0eaf5914d3d578eed1c2777a066dc390f37a321a
joe <31982488+Youssef-Shehata@users.noreply.github.com>
2024-08-03 11:37:56 +0300

    first

--- src/main.rs
+++ src/main.rs
@@ -26,9 +26,9 @@ fn main() {
     io::stdin().read_line(&mut input_line).unwrap();
 
     // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }
