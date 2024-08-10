a4b730d6ef8814945fa1efab92470ff674244f64
Youssef-Shehata <31982488+Youssef-Shehata@users.noreply.github.com>
2024-08-10 20:00:16 +0300

    refactoring 2 , added + quantifier

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ enum WhatWematchin {
     BeginningOfLine(String),
     Group(String),
     Symbol(String),
+    Quantifier(String),
 }
 impl Config {
     pub fn new(input: &[String]) -> Result<Config, &'static str> {
@@ -33,7 +34,7 @@ impl Config {
             let mut res: Vec<String> = Vec::new();
             for letter in temp.chars() {
                 match letter {
-                    mark if mark == '+' || mark == '?' => {
+                    mark if mark == '+' => {
                         let quantifier = res.pop().unwrap_or(String::from(""));
                         if !res.is_empty() {
                             final_pat.push(res.join(""));
@@ -100,6 +101,8 @@ impl Config {
                 pattern_enum.push(WhatWematchin::EndOfLine(String::from(pat)));
             } else if pat.starts_with("[") {
                 pattern_enum.push(WhatWematchin::Group(String::from(pat)));
+            } else if pat.ends_with("+") {
+                pattern_enum.push(WhatWematchin::Quantifier(String::from(pat)));
             } else {
                 pat.split("").filter(|&x| x != "").for_each(|x| {
                     pattern_enum.push(WhatWematchin::Exact(String::from(x)));
@@ -112,8 +115,293 @@ impl Config {
 }
 pub fn grep(input: &str, pattern: &str) -> bool {
     println!("Matching {} With {}", input, pattern);
+    let mut i = 0;
     let pattern = Config::serialize_pattern(pattern);
+    while i < input.len() {
+        for (index, pat) in pattern.iter().enumerate() {
+            let global_flag: bool = match pat {
+                WhatWematchin::Exact(pattern) => {
+                    match_exact(input.chars().nth(i).unwrap(), &pattern, &mut i)
+                }
 
-    println!("patter : {:?}", pattern);
+                WhatWematchin::EndOfLine(pattern) => line_matches(&input, &pattern, true, &mut i),
+                WhatWematchin::BeginningOfLine(pattern) => {
+                    line_matches(&input, &pattern, false, &mut i)
+                }
+                WhatWematchin::Group(pattern) => {
+                    match_group(input.chars().nth(i).unwrap(), &pattern, &mut i)
+                }
+
+                WhatWematchin::Symbol(pattern) => {
+                    match_symbol(input.chars().nth(i).unwrap(), &pattern, &mut i)
+                }
+                WhatWematchin::Quantifier(pattern) => match_quantifier(&input, &pattern, &mut i),
+            };
+
+            if global_flag && index == pattern.len() - 1 {
+                return true;
+            } else if !global_flag || i == input.len() {
+                break;
+            }
+        }
+    }
     return false;
+}
+fn match_exact(input: char, pattern: &str, i: &mut usize) -> bool {
+    *i += 1;
+    println!("matching {} with exactly {}", input, pattern);
+
+    return input == pattern.chars().nth(0).unwrap();
+}
+fn line_matches(input: &str, pattern: &str, end: bool, i: &mut usize) -> bool {
+    *i += 1;
+    let mut input: Vec<&str> = input.split("").collect();
+    let mut pattern: Vec<&str> = pattern.split("").collect();
+    if end {
+        input.reverse();
+        pattern.reverse();
+    }
+    let input = input.join("");
+    let pattern = pattern.join("");
+
+    println!("matchin line {input} with {pattern}");
+    for (mut i, ch) in pattern[1..].chars().enumerate() {
+        if let Some(x) = input.chars().nth(i) {
+            if x != ch {
+                if end {
+                    i = input.len() - i;
+                }
+                return false;
+            }
+        } else {
+            if end {
+                i = input.len() - i;
+            }
+            return false;
+        }
+    }
+    return true;
+}
+fn match_group(input: char, pattern: &str, i: &mut usize) -> bool {
+    *i += 1;
+    println!("matching {} with group {}", input, pattern);
+
+    if pattern.starts_with("[^") {
+        let pattern = &pattern[2..pattern.len() - 1];
+        if pattern.contains(input) {
+            return false;
+        };
+        return true;
+    } else {
+        let pattern = &pattern[1..pattern.len() - 1];
+        if pattern.contains(input) {
+            return true;
+        };
+        return false;
+    }
+}
+fn match_symbol(input: char, pattern: &str, i: &mut usize) -> bool {
+    println!("matching {} with symbol {}", input, pattern);
+
+    *i += 1;
+    match pattern {
+        r#"\w"# => {
+            if input.is_alphanumeric() {
+                return true;
+            }
+            false
+        }
+        r#"\d"# => {
+            if input.is_digit(10) {
+                return true;
+            }
+            false
+        }
+        _ => false,
+    }
+}
+fn match_quantifier(input: &str, pattern: &str, i: &mut usize) -> bool {
+    let target = pattern.chars().nth(0).unwrap();
+    match pattern {
+        pat if pat.ends_with("+") => {
+            println!(
+                "matching plus {} with {target}",
+                input.chars().nth(*i).unwrap()
+            );
+            if !(input.chars().nth(*i).unwrap() == target) {
+                return false;
+            }
+            while *i < input.len() && input.chars().nth(*i).unwrap() == target {
+                *i += 1;
+            }
+            return true;
+        }
+        _ => {
+            return false;
+        }
+    }
+}
+//--------------------------------------------------------------//
+//TESTS
+//______________________________________________________________//
+
+#[cfg(test)]
+pub mod exact {
+    use super::*;
+
+    #[test]
+    fn case1() {
+        assert_eq!(grep("wd", "d"), true);
+    }
+
+    #[test]
+    fn case2() {
+        assert!(grep("ass", "ass"));
+    }
+}
+#[cfg(test)]
+
+pub mod symbols {
+    use super::*;
+    #[test]
+    fn case3() {
+        assert!(grep("2", "\\d"));
+    }
+    #[test]
+    fn case4() {
+        assert!(grep("012", "\\d\\d\\d"));
+    }
+}
+#[cfg(test)]
+pub mod groups {
+    use super::*;
+
+    #[test]
+    fn case5() {
+        assert_ne!(grep("w29d", "[sa]"), true);
+    }
+    #[test]
+    fn case6() {
+        assert!(grep("oopspp", "[so]"));
+    }
+    #[test]
+    fn case7() {
+        assert!(grep("019248apapopopiw23", "[^nmbv]"));
+    }
+    #[test]
+    fn case8() {
+        assert!(grep("qwe", "[sw]"));
+    }
+}
+#[cfg(test)]
+pub mod combinations {
+
+    use super::*;
+    #[test]
+    fn case9() {
+        assert!(grep("d2d apple", "\\w\\d\\w apple"));
+    }
+    #[test]
+    fn case10() {
+        assert!(grep("22w a", "\\d\\dw [sa]"));
+    }
+    #[test]
+    fn case11() {
+        assert_ne!(grep("opac", "[^c]"), true);
+    }
+    #[test]
+    fn case15() {
+        assert_ne!(grep("dsx", "d[pw]x"), true);
+    }
+    #[test]
+    fn case16() {
+        assert_ne!(grep("12 ds 21", "12 ds [^2]1"), true);
+    }
+    #[test]
+    fn case17() {
+        assert_ne!(grep("22w ", "\\d\\dw [^sa]"), true);
+    }
+}
+#[cfg(test)]
+pub mod beginning_of_line {
+    use super::*;
+    #[test]
+    fn case12() {
+        assert_eq!(grep("opac", "^opa"), true);
+    }
+    #[test]
+    fn case13() {
+        assert_eq!(grep("opac", "^o"), true);
+    }
+    #[test]
+    fn case14() {
+        assert_eq!(grep("a", "^a"), true);
+    }
+    #[test]
+    fn case18() {
+        assert_ne!(grep("da", "^das"), true);
+    }
+    #[test]
+    fn case19() {
+        assert_ne!(grep("ad", "^d"), true);
+    }
+    #[test]
+    fn case20() {
+        assert_ne!(grep("1p", "^1 "), true);
+    }
+    #[test]
+    fn case21() {
+        assert_ne!(grep("daas", "^aas"), true);
+    }
+    #[test]
+    fn case22() {
+        assert_ne!(grep("slog", "^log"), true);
+    }
+}
+
+#[cfg(test)]
+pub mod end_of_line {
+    use super::*;
+
+    #[test]
+    fn case23() {
+        assert_ne!(grep("man ", "man$"), true);
+    }
+    #[test]
+    fn case24() {
+        assert!(grep("o", "o$"));
+    }
+    #[test]
+    fn case25() {
+        assert!(grep("mad man", "man$"));
+    }
+    #[test]
+    fn case26() {
+        assert!(grep("qwe  ", "  $"));
+    }
+}
+#[cfg(test)]
+pub mod plus {
+    use super::*;
+
+    #[test]
+    fn case27() {
+        assert_eq!(grep("man ", "ma+n"), true);
+    }
+    #[test]
+    fn case28() {
+        assert_eq!(grep("maan ", "ma+n"), true);
+    }
+    #[test]
+    fn case29() {
+        assert_ne!(grep("mn ", "ma+n"), true);
+    }
+    #[test]
+    fn case30() {
+        assert_eq!(grep("aan ", "a+n"), true);
+    }
+    #[test]
+    fn case31() {
+        assert_eq!(grep("maa ", "ma+"), true);
+    }
 }
